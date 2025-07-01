0ba688ebfeaf4275e12b4b3000af115051c2c1a6
nayan soni <201b165@juetguna.in>
2025-07-02 00:50:57 +0530

    put animation in slider

--- lib/component/custom_bottom_navigation_button.dart
+++ lib/component/custom_bottom_navigation_button.dart
@@ -1,61 +1,99 @@
 import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CustomBottomNavigationButton extends StatelessWidget {
+class CustomBottomNavigationButton extends StatefulWidget {
   const CustomBottomNavigationButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return
-       Container (
-        height: 50,
-        width: 180,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(24),
-          color: Colors.white,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 6.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              buildNavigationText("Expense"),
-              Text("Analysis"),
-            ],
-          ),
-        ),
-      );
-    }
+  State<CustomBottomNavigationButton> createState() =>
+      _CustomBottomNavigationButtonState();
+}
 
-  Container buildNavigationText(String text) {
+class _CustomBottomNavigationButtonState
+    extends State<CustomBottomNavigationButton> {
+  bool isExpenseSelected = true;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
+      height: 50,
+      width: 180,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(24),
-        border: Border.all(color: Colors.grey[200]!, width: 1),
-        color: Colors.grey[100]!, // Semi-transparent background
+        color: Colors.white,
       ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(24),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
-            child: Center(
-              child: Text(
-                text,
-                style: GoogleFonts.saira(
-                  color: Colors.black,
+      child: Stack(
+        children: [
+          // Sliding Glassy Effect
+          AnimatedAlign(
+            alignment: isExpenseSelected
+                ? Alignment.centerLeft
+                : Alignment.centerRight,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            child: Container(
+              width: 85,
+              height: 40,
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24),
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.grey[300]!),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(24),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                  child: const SizedBox(), // Glass effect only
                 ),
               ),
+            ),
+          ),
+
+          // Static Text Labels with Tap
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildTab(text: "Expense", isSelected: isExpenseSelected, onTap: () {
+                setState(() => isExpenseSelected = true);
+              }),
+              _buildTab(text: "Analysis", isSelected: !isExpenseSelected, onTap: () {
+                setState(() => isExpenseSelected = false);
+              }),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTab({
+    required String text,
+    required bool isSelected,
+    required VoidCallback onTap,
+  }) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Center(
+          child:isSelected? Text(
+            text,
+            style: GoogleFonts.saira(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          ):Text(
+            text,
+            style: GoogleFonts.saira(
+              fontSize: 14,
+               fontWeight: FontWeight.w600,
+              color: Colors.grey[600],
             ),
           ),
         ),
       ),
     );
   }
-
-  }
-
+}
